fc08a746a947d4a16e289788760ba9561d3cd788
Kevin C Malin <racmalin@yahoo.com>
2020-04-21 02:49:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,13 +3,13 @@
 
 def countdown(int)
   
-#   while int < 0 
-#   puts "#{int} SECOND(S)!"
+  while int < 0 
+  puts "#{int} SECOND(S)!"
     
-#     int -= 1 
-#   "HAPPY NEW YEAR!"
+    int -= 1 
+  "HAPPY NEW YEAR!"
   
-# end
+end
 end
 
 # def countdown_with_sleep(int)
