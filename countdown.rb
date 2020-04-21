7e75486a1ab8c706ea5ff82bb50820da9b6fa0dd
Kevin C Malin <racmalin@yahoo.com>
2020-04-21 01:16:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+x = 10
 
 def countdown
+ while x > 0 
+   puts "#{x} SECOND(S)!"
+   x -= 1
+   puts "HAPPY NEW YEAR!"
+ end
 end
+
+# def countdown_with_sleep
+#   # 5.seconds 
+# end
