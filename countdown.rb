b8b16742c059af9cd71d3ea5704e0568957e040f
Kevin C Malin <racmalin@yahoo.com>
2020-04-21 02:00:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 # def x
 # end
 
-def countdown(x)
-  puts ""
+# def countdown(x)
+#   puts ""
   
-  while x < 0 
+#   while x < 0 
   
-  x -= 1 == "HAPPY NEW YEAR!"
- end
-end
+#   x -= 1 == "HAPPY NEW YEAR!"
+# end
+# end
 
 def countdown_with_sleep(int)
     countdown(int)
